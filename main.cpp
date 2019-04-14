24b49b72190f4d4a81333fe9c266f8a72daefad0
kwh44 <maistruk44@gmail.com>
2019-04-14 20:51:42 +0300

    commit #1

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/*
+  read config file from command line:
+    if config file given then read it else search for config.dat in pwd.
+    - read input filename, output filenames, number of threads to utilize;
+      input data
+        -- input file format: .txt, .zip with .txt files, .tar, gz, tar.gz
+        -- character encoding utf-8, en_us locale
+  process:
+    count how many time each word in file/s was used;
+     -- count without delimiters on each side, delimiters inside a word is allowed;
+     -- uppercase = lowercase; ignore the register;
+    exception handling;
+     --
+    -
+  output result to two files
+  */
+
+
+int main(int argc, char **argv) {
+
+  return 0;
+}
