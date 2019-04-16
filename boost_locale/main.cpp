d7af27432b4d4ae78d87481a2b5123c1a0978ee9
kwh44 <maistruk44@gmail.com>
2019-04-16 18:02:58 +0300

    fold_case/normalize added

--- boost_locale/main.cpp
+++ boost_locale/main.cpp
@@ -1,27 +1,14 @@
-#include <boost/locale.hpp>
-#include <boost/locale/conversion.hpp>
 #include <iostream>
+#include <string>
 #include <boost/locale/generator.hpp>
-
-using namespace boost::locale;
+#include <boost/locale.hpp>
 
 int main() {
-    std::cout << "I am here\n";
-    generator gen;
-    auto loc = gen("en_US.UTF-8");
-    std::string text{"Building CXX object"};
-    std::cout << text << std::endl;
+    boost::locale::generator gen;
+    std::locale::global(gen("en_us.UTF-8"));
+    std::string text = "paralleO GRÜSSEN InDexing";
     auto normalized_text = boost::locale::fold_case(
-            boost::locale::normalize(text, boost::locale::norm_type::norm_default, gen("en_US.UTF-8")));
-
-    /*
-    std::locale::global(gen("en_US.UTF-8"));
-    std::wstring a = L"Façade", b = L"façade";
-    bool eq = 0 == std::use_facet<collator<wchar_t> >(loc).compare(collator_base::secondary, a, b);
-    std::wcout << a << L" and " << b << L" are " << (eq ? L"identical" : L"different") << std::endl;
-    std::cout << boost::locale::as::spellout;
-    std::cout << 22 << std::endl;
-    */
-
+            boost::locale::normalize(text));
+    std::cout << normalized_text << std::endl;
     return 0;
 }
