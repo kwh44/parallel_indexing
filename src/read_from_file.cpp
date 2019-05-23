c7d097cd83ef3ee9a34714c081a6f5f41b5ab1ca
kwh44 <maistruk44@gmail.com>
2019-05-23 15:08:04 +0300

    fixed bug with reading from archive

--- src/read_from_file.cpp
+++ src/read_from_file.cpp
@@ -25,27 +25,27 @@ void get_file_content(std::vector<std::string> &storage_container, std::string &
     } else {
         int response;
         ssize_t len;
-        constexpr size_t buffer_size = 8192;
-        char buff[buffer_size];
-        struct archive *a;
+		int64_t offset;
+		const void *buff;
+		size_t size;
+		struct archive *a;
         struct archive_entry *entry;
-        a = archive_read_new();
+		a = archive_read_new();
         archive_read_support_filter_all(a);
         archive_read_support_format_all(a);
-        if ((response = archive_read_open_filename(a, filename.c_str(), buffer_size))) {
+		if ((response = archive_read_open_filename(a, filename.c_str(), 10240))) {
             std::cerr << "archive_read_open_filename() failed: " << archive_error_string(a) << std::endl;
             std::exit(response);
         }
         while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {
-            if (archive_entry_size(entry) > 0) {
-                std::stringstream entry_content_string;
-                len = archive_read_data(a, buff, sizeof(buff));
-                while (len > 0) {
-                    entry_content_string << boost::locale::fold_case(boost::locale::normalize(std::string(buff)));
-                    len = archive_read_data(a, buff, sizeof(buff));
-                }
-                storage_container.emplace_back(std::move(entry_content_string.str()));
-            }
+                std::stringstream content;
+                response = archive_read_data_block(a, &buff, &size, &offset);
+				while (response != ARCHIVE_EOF && response == ARCHIVE_OK) {
+					char * buffer = static_cast<char *>(const_cast<void*>(buff));
+					content << boost::locale::fold_case(boost::locale::normalize(std::string(buffer,size)));
+					response = archive_read_data_block(a, &buff, &size, &offset);
+				}
+                storage_container.emplace_back(std::move(content.str()));
         }
         archive_read_close(a);
         archive_read_free(a);
--- src/read_txt_to_string.cpp
+++ src/read_txt_to_string.cpp
@@ -1,11 +1,9 @@
 #include "read_txt_to_string.hpp"
 #include <limits>
+#include <boost/locale.hpp>
 
 void read_from_txt(std::ifstream &file, std::string &text) {
-    auto const start_pos = file.tellg();
-    file.ignore(std::numeric_limits<std::streamsize>::max());
-    auto const char_count = file.gcount();
-    file.seekg(start_pos);
-    text = std::string(char_count, char{});
-    file.read(&text[0], text.size());
+    auto ss = std::ostringstream{};
+    ss << file.rdbuf();
+    text = boost::locale::fold_case(boost::locale::normalize(ss.str()));
 }
